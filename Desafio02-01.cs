03ea9aad5b5d28643a13c02e01f1d977039536e9
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-10 21:20:37 -0500

    Add files via upload

---
+++ Desafio02-01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio0201
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Aqui voy a escrubir las variables...
+            Console.WriteLine("Ingrese el valor de y, z: ");
+            double y = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+
+            //Aqui calculamos el angulo c
+            double anguloc = Math.Atan2(z, y) * (180.0 / Math.PI);
+            Console.WriteLine("El valor del angulo c es: " + anguloc);
+
+            //Aqui calculamos el angulo a
+            double anguloa = Math.Atan2(y, z) * (180.0 / Math.PI);
+            Console.WriteLine("El valor del angulo a es: " + anguloa);
+
+            //Aqui calculamos el lado t
+            double ladot = Math.Sqrt(Math.Pow(z, 2) + Math.Pow(y, 2));
+            Console.WriteLine("El valor del lado t es: " + ladot);
+        }
+    }
+}
