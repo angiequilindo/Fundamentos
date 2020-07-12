58a8b5f481e4e061f06cae86e8d4c42bf5735a0e
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-11 23:28:52 -0500

    Add files via upload

---
+++ Desafio02-03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desafio0203
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Aqui se ingresan los valores...
+            Console.WriteLine("Ingrese el valor de z: ");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de c en grados: ");
+            double c = double.Parse(Console.ReadLine());
+
+            //Aqui calculamos el angulo a
+            double anguloa = 90.0 - c;
+            Console.WriteLine("El valor del angulo a es: " + anguloa);
+
+            //Aqui calculamos el lado y
+            double ladoy = z / Math.Tan(Math.PI * c / 180.0);
+            Console.WriteLine("El valor del lado y es: " + ladoy);
+
+            //Aqui calculamos el lado t
+            double ladot = z/Math.Sin(Math.PI * c / 180.0);
+            Console.WriteLine("El valor del lado t es: " + ladot);
+
+
+        }
+    }
+}
