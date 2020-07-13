c5dd34726644c98c995554efba6b6f9b60d56d82
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-13 10:15:56 -0500

    Add files via upload

---
+++ Desafio01-044.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio01044
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese el angulo c,  y los lados w, t");
+            double cgrados = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double t = double.Parse(Console.ReadLine());
+
+            //Conversion de los angulos a radianes
+            double c = cgrados * Math.PI / 180.0;
+
+            //Lados del triangulo pequeno
+            double z = t * Math.Sin(c); 
+            double y = t * Math.Cos(c); 
+
+            //Angulo d
+            double d = Math.Asin(z / w); //no paso a radianes porque son dos lados 
+
+            //Lado x
+            double x = ((Math.Cos(d) * w) - y);
+
+            Console.WriteLine("El valor del lado x es:" + x);
+
+                
+        }
+    }
+}
