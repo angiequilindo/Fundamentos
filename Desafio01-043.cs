75d373ce8f0db326d02822fe5cb570a05a2ff5e7
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-13 00:24:18 -0500

    Add files via upload

---
+++ Desafio01-043.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio01043
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese el angulo d,  y los lados w, x");
+            double dgrados = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+
+            //Conversion de los angulos a radianes
+            double d = dgrados * Math.PI / 180.0;
+
+            double ladoy = (Math.Cos(d) * w) - x;
+            Console.WriteLine("El valor del lado y es:" + ladoy);
+        }
+    }
+}
