7a4335c39dbe8a563571fe0087d6969461665c5c
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-12 23:46:14 -0500

    Add files via upload
    programa 2

---
+++ Desafio01-042.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio01042
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese el angulo b, d y el lado y");
+            double bgrados = double.Parse(Console.ReadLine());
+            double dgrados = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            //Conversion de los angulos a radianes
+            double b = bgrados * Math.PI / 180.0;
+            double d = dgrados * Math.PI / 180.0;
+            double anguloa = (90- bgrados - dgrados)* Math.PI / 180.0;
+
+            double z = y /Math.Tan(anguloa);
+            Console.WriteLine("El valor del lado z es:"+ z);
+
+
+        }
+    }
+}
