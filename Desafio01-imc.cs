1657263eaf3be20b690d7d77817bc6663c50ace8
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-15 18:37:10 -0500

    Add files via upload

---
+++ Desafio01-imc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Desafio01IMC
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su peso (kg) y su estatura (m)");
+            double peso = double.Parse(Console.ReadLine());
+            double estatura = double.Parse(Console.ReadLine());
+
+            double imc = peso / Math.Pow(estatura, 2);
+            Console.WriteLine("Su imc es" + imc);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Peso inferior al normal");
+            }
+            else if (18.5 <= imc && imc <= 24.9)
+            {
+                Console.WriteLine("Peso normal");
+            }
+            else if (25.0 <= imc && imc <= 29.9)
+            {
+                Console.WriteLine("Peso superior al normal");
+            }
+            else if (imc >= 30.0)
+            {
+
+                Console.WriteLine("Obesidad");
+            }
+
+
+
+        }
+
+
+
+    }
+}
