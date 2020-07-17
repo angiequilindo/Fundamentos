627a920e9fd33301685c0b554ee5598c75dbca76
angiequilindo <68124641+angiequilindo@users.noreply.github.com>
2020-07-17 00:26:20 -0500

    Add files via upload

---
+++ CalculoTarifa01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Desafio01CalculoDeTarifa
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+
+            int smmlv = 877803;
+
+            if (salario < (2 * smmlv))
+
+            {
+
+                Console.WriteLine("Su tarifa es A");
+
+            }
+
+            else if (salario >= (2 * smmlv) && salario < (4 * smmlv))
+
+            {
+
+                Console.WriteLine("Su tarifa es B ");
+
+            }
+
+            else if (salario >= (4 * smmlv))
+
+            {
+
+                Console.WriteLine("Su tarifa es C");
+            }
+        }
+
+    }
+}
